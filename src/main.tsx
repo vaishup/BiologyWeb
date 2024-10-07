cbab3273f4f54c2eb55adf0ca468489e238206f6
vaishali68 <vaishalipanchal6899@gmail.com>
2024-10-07 18:11:33 -0400

    chnages

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './css/satoshi.css';
 import 'jsvectormap/dist/css/jsvectormap.css';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Amplify } from 'aws-amplify';
+
 import config from './amplifyconfiguration.json';
 
 Amplify.configure(config);
